3eaa0101c4fd59165995bbbbc890a85a5bb27cf7
VinodGit143 <vinodisemvjce@gmail.com>
2025-03-26 03:02:27 +0530

    Updated app.js and index.html and style.css

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const video2= document.getElementById('projectVideo2');
 const video3= document.getElementById('projectVideo3');
 const hoverSign= document.querySelector('.hover-sign');
 
+//sidebar elements //
+
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const close = document.querySelector('.close-icon');
+
 
 const videoList=[video1, video2, video3];
 
@@ -15,4 +21,20 @@ videoList.forEach(function(video){
         video.pause();
         hoverSign.classList.remove('active');
     })
+})
+
+
+//sidebar elements //
+
+
+menu.addEventListener('click', function(){
+    sideBar.classList.remove('close-sidebar');
+    sideBar.classList.add('open-sidebar');
+})
+
+
+
+close.addEventListener('click', function(){
+    sideBar.classList.remove('open-sidebar');
+    sideBar.classList.add('close-sidebar');
 })
